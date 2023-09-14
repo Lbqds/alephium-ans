cc57f37916313aa9d4367a66ed29f40e5a29fdf2
lbqds <lbqds@outlook.com>
2023-09-14 10:38:04 +0800

    Fix contract deployment scripts

--- scripts/00_deploy_primary_registrar.ts
+++ scripts/00_deploy_primary_registrar.ts
@@ -1,15 +1,19 @@
 import { Deployer, DeployFunction, Network } from '@alephium/cli'
 import { Settings } from '../alephium.config'
-import { PrimaryRegistrar, PrimaryRegistrarTypes, PrimaryRecord } from '../artifacts/ts'
+import { PrimaryRegistrar, PrimaryRegistrarTypes, PrimaryRecord, RecordToken } from '../artifacts/ts'
 
 const deployPrimaryRegistrar: DeployFunction<Settings> = async (deployer: Deployer, network: Network<Settings>): Promise<void> => {
   if (deployer.account.group !== network.settings.primaryGroup) {
     return
   }
   const recordTemplateResult = await deployer.deployContract(PrimaryRecord, { initialFields: PrimaryRecord.getInitialFieldsWithDefaultValues() })
+  const recordTokenTemplateResult = await deployer.deployContract(RecordToken, {
+    initialFields: { registrar: '', name: '' }
+  })
   const initialFields: PrimaryRegistrarTypes.Fields = {
     registrarOwner: network.settings.registrarOwner,
-    recordTemplateId: recordTemplateResult.contractInstance.contractId
+    recordTemplateId: recordTemplateResult.contractInstance.contractId,
+    recordTokenTemplateId: recordTokenTemplateResult.contractInstance.contractId
   }
   const result = await deployer.deployContract(PrimaryRegistrar, { initialFields: initialFields })
   network.settings.primaryRegistrarAddress = result.contractInstance.address
